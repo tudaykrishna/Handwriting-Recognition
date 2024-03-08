1d20e7d24dcf5f259f8304ab700c90da8a824435
T Uday Krishna <96607888+tudaykrishna@users.noreply.github.com>
2024-03-08 11:48:05 +0530

    2

---
+++ config.py
@@ -0,0 +1,16 @@
+import stow
+from datetime import datetime
+from mltu.configs import BaseModelConfigs
+
+class ModelConfig(BaseModelConfigs):
+    def __init__(self):
+        super().__init__()
+        self.model_path = stow.join('/path/',datetime.strftime(datetime.now(),"%Y%m%d%H%M"))
+        self.vocab = ''
+        self.height = 32
+        self.width = 128
+        self.max_text_length = 0
+        self.batch_size = 16
+        self.learning_rate = 0.0005
+        self.train_epochs = 1000
+        self.train_workers = 20
